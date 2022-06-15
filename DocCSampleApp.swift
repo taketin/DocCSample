0daba46971da009c7d058e6a41b5aaa9080fc9d9
Hidenori TAKESHITA <taketin@linecorp.com>
2022-06-16 01:05:31 +0900

    Initial commit

---
+++ DocCSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocCSampleApp.swift
+//  DocCSample
+//
+//  Created by ST14573 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct DocCSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
